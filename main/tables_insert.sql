cc2bf593e60c9789979a6f21444282f0812ad5e8
Ishan-Mittal <ishan20.mittal@gmail.com>
2021-10-09 23:31:20 +0800

    Added Cart functionality and Database files

---
+++ main/tables_insert.sql
@@ -0,0 +1,15 @@
+use f32ee;
+
+insert into shows values
+  (1, "Hamilton", "Playing"),
+  (2, "Chicago", "Playing"),
+  (3, "The Phantom of the Opera", "Playing"),
+  (4, "The Lion King", "Playing"),
+  (5, "Les Miserables", "Playing"),
+  (6, "Wicked", "Playing");
+  
+insert into schedule values
+  (1, 1, "2021-10-25 19:30:00", 20, 30, 40, 50),
+  (2, 1, "2021-10-26 20:30:00", 20, 30, 40, 50),
+  (3, 1, "2021-10-27 13:30:00", 20, 30, 40, 50),
+  (4, 1, "2021-10-28 17:30:00", 20, 30, 40, 50);
