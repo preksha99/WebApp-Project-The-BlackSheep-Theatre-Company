a8f495325d717de2878385e70cb0f0b8d2582fa4
Ishan-Mittal <ishan20.mittal@gmail.com>
2021-10-24 22:30:56 +0800

    Finalized Chicoago.php

--- main/chicago_old.php
+++ main/chicago_old.php
@@ -11,16 +11,15 @@ if (!isset($_SESSION))  {
 		$_SESSION['cart_item'] = array();
 	}
 	#unset($_SESSION["cart_item"]);
-	#var_dump($_SESSION);
+	var_dump($_SESSION);
 }
 $id = session_id();
-#echo "<br>Session id = $id <br>";
+echo "<br>Session id = $id <br>";
 
 if (isset($_POST["buy"])) {
 	$itemArray = array("showid"=>$_POST["showid"], "scheduleid"=>$_POST["scheduleid"], "quantity_area1"=>$_POST['quantity_area1'], 
 	"quantity_area2"=>$_POST['quantity_area2'], "quantity_area3"=>$_POST['quantity_area3'], "quantity_area4"=>$_POST['quantity_area4']);
 	
-	var_dump ($itemArray);
 	if (($_POST['quantity_area1']==0)&&($_POST['quantity_area2']==0)&&($_POST['quantity_area3']==0)&&($_POST['quantity_area4']==0)) {
 		$all_zero = true;
 	}
@@ -75,7 +74,7 @@ if (isset($_POST["buy"])) {
 			<?php
 		}
 	}
-	#echo '<pre>'.print_r($_SESSION, TRUE).'</pre>';
+	echo '<pre>'.print_r($_SESSION, TRUE).'</pre>';
 
 	#$update_query = "UPDATE schedule SET quantity_area1 = quantity_area1-".$itemArray["quantity_area1"].", quantity_area2 = quantity_area2-".$itemArray["quantity_area2"].", 
 	#quantity_area3 = quantity_area3-".$itemArray["quantity_area3"].", quantity_area4= quantity_area4-".$itemArray["quantity_area4"]." WHERE showid=".$_POST["showid"]. " and scheduleid=".$_POST["scheduleid"].";";
@@ -85,20 +84,18 @@ if (isset($_POST["buy"])) {
 	$retrieval_query = "SELECT * FROM schedule WHERE showid=".$_POST["showid"]. " and scheduleid=".$_POST["scheduleid"];
 	$retrieval_result = $db->query($retrieval_query);
 	$retrieved_row=mysqli_fetch_assoc($retrieval_result);
-	#echo '<pre>'; print_r($retrieved_row); echo '</pre>';
+	echo '<pre>'; print_r($retrieved_row); echo '</pre>';
 	
 }
 unset ($_POST["buy"]);
 ?>
-<!DOCTYPE html>
 <html lang="en">
     <head>
         <title>The BlackSheep Theatre Company</title>
         <link rel="stylesheet" href="style.css">
         <meta charset="utf-8">
-        <style>
-            
-            .row {
+		<style>
+			.row {
                 display: flex;
             }
             .left{
@@ -267,7 +264,7 @@ unset ($_POST["buy"]);
                 flex: 33.33%;
             }
 
-            .submitbutton {
+            .cart-button {
                 margin-left: 55px;
                 margin-top: 0px;
                 background-color: #292121;
@@ -299,7 +296,7 @@ unset ($_POST["buy"]);
                 margin-right: 20px;
             }
 
-            .price-qty #quantity_area1,#quantity_area2,#quantity_area3,#quantity_area4 {
+            .price-qty #qty {
                 width: 140px;
                 height: 32px;
                 background-color: #E5E5E5;
@@ -330,28 +327,28 @@ unset ($_POST["buy"]);
                 text-align: center;
                 color: #FFFFFF;
             }
-
-            </style>
+		</style>
     </head>
     <body>
-        <nav>
-            <ul id="nav">
-                <a style="float:left" href="#">
-                    <div class="logo-image">
-                          <img src="images/logo-image.png">
-                    </div>
-              </a>
-                <li><a href="home.html">Home</a>&nbsp;</li>
-                <li><a class="active" href="plays.html">Plays</a>&nbsp;<li>
-                <li><a href="contact.html">Contact Us</a>&nbsp;</li>
-                <a style="float:right" href="cart.php">
-                    <div class="cart-image">
-                          <img src="images/cart-image.png">
-                    </div>
-              </a>
-            </ul>
-        </nav>
-        <br>
+            <nav>
+                <ul id="nav">
+                    <a style="float:left" href="#">
+                        <div class="logo-image">
+                              <img src="images/logo-image.png">
+                        </div>
+                  </a>
+                    <li><a href="home.html">Home</a>&nbsp;</li>
+				    <li><a class="active" href="plays.html">Plays</a>&nbsp;<li>
+				    <li><a href="contact.html">Contact Us</a>&nbsp;</li>
+                    <a style="float:right" href="cart.php">
+                        <div class="cart-image">
+                              <img src="images/cart-image.png">
+                        </div>
+                  </a>
+                </ul>
+            </nav>
+			<br>
+			<br>
         <div class = "playDescription">
             <div class="row">
                 <div class="column left">
@@ -409,39 +406,39 @@ unset ($_POST["buy"]);
             <p id="seating">Seating Chart</p>
             <img src="images/seating.png" width=500px height=581px align="center">
         </div>
-        <div class="tickets">
-            <p id="available">Available Tickets</p>
-            <a name="A"></a>
-			<?php 
-				$date = date('Y-m-d H:i:s');
-				$query = "SELECT * FROM schedule WHERE schedule.datetime >= '".$date."' and showid=2";
-				$result = $db->query($query);
-				$num_results = $result->num_rows;
-				while($row=mysqli_fetch_assoc($result)) {
-					$resultset[] = $row;
-				}
-				for ($i=0; $i < $num_results; $i++) {
+		
+			<div id="headingtext">
+				<p>Available Showtimes</p>
+			</div>
+			<?php
+			$date = date('Y-m-d H:i:s');
+			$query = "SELECT * FROM schedule WHERE schedule.datetime >= '".$date."' and showid=2";
+			$result = $db->query($query);
+			$num_results = $result->num_rows;
+			while($row=mysqli_fetch_assoc($result)) {
+				$resultset[] = $row;
+			}
+			#echo '<pre>'; print_r($resultset); echo '</pre>';
+			#var_dump($resultset[0]["datetime"]);
+			
+			?>
+			<div id="content">
+				<table>
+				<?php for ($i=0; $i < $num_results; $i++) {
 					$show_datetime = DateTime::createFromFormat('Y-m-d H:i:s', $resultset[$i]["datetime"]);
 					$show_date = $show_datetime->format('jS F Y');
 					$show_time = $show_datetime->format('h:i a');
-			?>
-            <div class="ticket-card">
-			<form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
-                <div class="ticket-card-header">
-                    <p id="date"><?php echo $show_date; ?></p>
-                </div>
-                <div class="row">
-                    <div class="ticket-column">
-                        <p id="time"><?php echo $show_time; ?></p>
-						<input type="submit" value="Add to Cart" class="submitbutton"></input>
-                        
-                    </div>
-                    <div class="ticket-column">
-                        <div class="price-qty">
-								<input type="hidden" name="scheduleid" value="<?php echo $resultset[$i]["scheduleid"]; ?>">
-								<input type="hidden" name="showid" value="<?php echo $resultset[$i]["showid"]; ?>">
-								<input type="hidden" name="buy" value="<?php echo "1"; ?>">
-                            <label for="quantity_area1">S$60:</label>
+				?>
+					<tr>
+						<td><?php echo $show_date."<br>"; 
+								echo $show_time."<br><br><br><br>"; 
+						?></td>
+						<td>
+						<form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
+							<input type="hidden" name="scheduleid" value="<?php echo $resultset[$i]["scheduleid"]; ?>">
+							<input type="hidden" name="showid" value="<?php echo $resultset[$i]["showid"]; ?>">
+							<input type="hidden" name="buy" value="<?php echo "1"; ?>">
+							<br><br><label for="quantity_area1">S$60:</label>
 							<select name="quantity_area1" id="quantity_area1">
 								<option value=0>0</option>
 								<?php if ($resultset[$i]["quantity_area1"]>=10) {
@@ -454,9 +451,8 @@ unset ($_POST["buy"]);
 								?>
 								<option value=<?php echo $x; ?>><?php echo $x; ?></option>
 								<?php } } ?>
-							</select></div>
-							<div class="price-qty">
-                            <label for="quantity_area2">S$45:</label>
+							</select>
+							<label for="quantity_area2">S$45:</label>
 							<select name="quantity_area2" id="quantity_area2">
 								<option value=0>0</option>
 								<?php if ($resultset[$i]["quantity_area2"]>=10) {
@@ -469,11 +465,8 @@ unset ($_POST["buy"]);
 								?>
 								<option value=<?php echo $x; ?>><?php echo $x; ?></option>
 								<?php } } ?>
-							</select></div>
-                    </div>
-                    <div class="ticket-column">
-						<div class="price-qty">
-                            <label for="quantity_area3">S$30:</label>
+							</select>
+							<label for="quantity_area3">S$35:</label>
 							<select name="quantity_area3" id="quantity_area3">
 								<option value=0>0</option>
 								<?php if ($resultset[$i]["quantity_area3"]>=10) {
@@ -486,9 +479,8 @@ unset ($_POST["buy"]);
 								?>
 								<option value=<?php echo $x; ?>><?php echo $x; ?></option>
 								<?php } } ?>
-							</select></div>
-						<div class="price-qty">
-                            <label for="quantity_area4">S$20:</label>
+							</select>
+							<label for="quantity_area4">S$20:</label>
 							<select name="quantity_area4" id="quantity_area4">
 								<option value=0>0</option>
 								<?php if ($resultset[$i]["quantity_area4"]>=10) {
@@ -501,43 +493,46 @@ unset ($_POST["buy"]);
 								?>
 								<option value=<?php echo $x; ?>><?php echo $x; ?></option>
 								<?php } } ?>
-							</select></div>
+							</select>
+							<td><input type="submit" value="Add to Cart" class="submitbutton"></input></td>
+						</form>
+						</td>
+					</tr>
+				<?php }
+				?>
+				</table>
+				<a href="cart.php" class="submitbutton">View Cart</a>
+			</div>
+			<br><br>
+            <footer>
+                <a style="float:left" href="#">
+                    <div class="logo-image-round">
+                          <img src="images/logo-image-round.png">
                     </div>
-                </div>
-				</form>
-            </div>
-				<?php } ?>
-            <button type="button" class="view-button"><a href="cart.php">View Cart</a></button>
-        </div>
-        <footer>
-            <a style="float:left" href="#">
-                <div class="logo-image-round">
-                      <img src="images/logo-image-round.png">
-                </div>
-          </a>
-          <div class="contact">
-              <p id="contact-title" style="float:left">Contact Us</p>
-              <br><br>
-              <ul>
-                  <li> 
-                    <img style="float:left" id="icon-image" src="images/location-image.png">
-                    &nbsp;&nbsp;&nbsp;1 Esplanade Dr, Singapore 038981
-                  </li>
-                  <br>
-                  <br>
-                  <li> 
-                    <img style="float:left" id="icon-image" src="images/phone-image.png">
-                    &nbsp;&nbsp;&nbsp;+65 7483 2289
-                  </li>
-                  <br>
-                  <br>
-                  <li> 
-                    <img style="float:left" id="icon-image" src="images/email-image.png">
-                    &nbsp;&nbsp;&nbsp;<a id="contact-email" href="mailto:info@blacksheeptheatre.com">info@blacksheeptheatre.com</a>
-                  </li>
-              </ul>
-              <p id="copyright">&copy; 2021 BlackSheep Theatre Company. All Rights Reserved.</p>
-          </div>
-        </footer>
+              </a>
+              <div class="contact">
+                  <p id="contact-title" style="float:left">Contact Us</p>
+                  <br><br>
+                  <ul>
+                      <li> 
+                        <img style="float:left" id="icon-image" src="images/location-image.png">
+                        &nbsp;&nbsp;&nbsp;1 Esplanade Dr, Singapore 038981
+                      </li>
+                      <br>
+                      <br>
+                      <li> 
+                        <img style="float:left" id="icon-image" src="images/phone-image.png">
+                        &nbsp;&nbsp;&nbsp;+65 7483 2289
+                      </li>
+                      <br>
+                      <br>
+                      <li> 
+                        <img style="float:left" id="icon-image" src="images/email-image.png">
+                        &nbsp;&nbsp;&nbsp;<a id="contact-email" href="mailto:info@blacksheeptheatre.com">info@blacksheeptheatre.com</a>
+                      </li>
+                  </ul>
+                  <p id="copyright">&copy; 2021 BlackSheep Theatre Company. All Rights Reserved.</p>
+              </div>
+            </footer>
     </body>
 </html>
